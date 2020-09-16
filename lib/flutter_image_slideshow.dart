af5da7e7b79aafa6c27dd189d1929715e1295ef7
edasandesu <nowtimber@gmail.com>
2020-09-16 22:46:28 +0900

    Add PageView

--- lib/flutter_image_slideshow.dart
+++ lib/flutter_image_slideshow.dart
@@ -1,7 +1,29 @@
 library flutter_image_slideshow;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+class FlutterImageSlideshow extends StatelessWidget {
+  FlutterImageSlideshow({this.images});
+  final List<Image> images;
+
+  final PageController _pageController = PageController(
+    viewportFraction: 1.0,
+    initialPage: 0,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: PageView.builder(
+        itemCount: images.length,
+        controller: _pageController,
+        itemBuilder: (context, index) {
+          return Container(
+            child: images[index],
+          );
+        },
+      ),
+    );
+  }
 }
