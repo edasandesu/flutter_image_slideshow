f9d2bdbdfa6b77e61b87d77d5514d3b9650a9ad3
edasandesu <nowtimber@gmail.com>
2020-09-17 20:11:54 +0900

    Add indicator

--- lib/flutter_image_slideshow.dart
+++ lib/flutter_image_slideshow.dart
@@ -1,28 +1,104 @@
 library flutter_image_slideshow;
 
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
-class FlutterImageSlideshow extends StatelessWidget {
-  FlutterImageSlideshow({this.images});
-  final List<Image> images;
+class ImageSlideshow extends StatefulWidget {
+  ImageSlideshow({
+    @required this.children,
+    this.width = double.infinity,
+    this.height = 200,
+    this.initialPage = 0,
+    this.indicatorColor = Colors.blue,
+    this.indicatorBackgroundColor = Colors.grey,
+  });
+  final List<Widget> children;
+  final double width;
+  final double height;
+  final int initialPage;
+  final Color indicatorColor;
+  final Color indicatorBackgroundColor;
 
-  final PageController _pageController = PageController(
-    viewportFraction: 1.0,
-    initialPage: 0,
-  );
+  @override
+  _ImageSlideshowState createState() => _ImageSlideshowState();
+}
+
+class _ImageSlideshowState extends State<ImageSlideshow> {
+  final StreamController<int> _onPageChanged =
+      StreamController<int>.broadcast();
+  PageController _pageController;
+
+  @override
+  void initState() {
+    _pageController = PageController(
+      initialPage: widget.initialPage,
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    _onPageChanged.close();
+    super.dispose();
+  }
+
+  Widget _indicator(BuildContext context) {
+    return Wrap(
+      spacing: 4,
+      runSpacing: 4,
+      alignment: WrapAlignment.center,
+      children: List<Widget>.generate(
+        widget.children.length,
+        (index) {
+          return StreamBuilder<int>(
+            initialData: _pageController.initialPage,
+            stream: _onPageChanged.stream.where(
+              (pageIndex) {
+                return index >= pageIndex - 1 && index <= pageIndex + 1;
+              },
+            ),
+            builder: (_, AsyncSnapshot<int> snapshot) {
+              return Container(
+                width: 6,
+                height: 6,
+                decoration: ShapeDecoration(
+                  shape: CircleBorder(),
+                  color: snapshot.data == index
+                      ? widget.indicatorColor
+                      : widget.indicatorBackgroundColor,
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-      height: 200,
-      child: PageView.builder(
-        itemCount: images.length,
-        controller: _pageController,
-        itemBuilder: (context, index) {
-          return Container(
-            child: images[index],
-          );
-        },
+      width: widget.width,
+      height: widget.height,
+      child: Stack(
+        children: [
+          PageView.builder(
+            onPageChanged: _onPageChanged.sink.add,
+            itemCount: widget.children.length,
+            controller: _pageController,
+            itemBuilder: (context, index) {
+              return widget.children[index];
+            },
+          ),
+          Positioned(
+            left: 0.0,
+            right: 0.0,
+            bottom: 10.0,
+            child: _indicator(context),
+          ),
+        ],
       ),
     );
   }
