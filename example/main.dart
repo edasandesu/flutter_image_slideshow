ff2ac7484b1bf0a1d9c906c8841c805d0934ffbc
edasandesu <nowtimber@gmail.com>
2020-11-16 20:26:25 +0900

    add example

---
+++ example/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Demo'),
+        ),
+        body: ImageSlideshow(
+          width: double.infinity,
+          height: 200,
+          initialPage: 0,
+          indicatorColor: Colors.blue,
+          indicatorBackgroundColor: Colors.grey,
+          children: [
+            Image.asset(
+              'images/sample_image_1.jpg',
+              fit: BoxFit.cover,
+            ),
+            Image.asset(
+              'images/sample_image_2.jpg',
+              fit: BoxFit.cover,
+            ),
+            Image.asset(
+              'images/sample_image_3.jpg',
+              fit: BoxFit.cover,
+            ),
+          ],
+          onPageChanged: (value) {
+            print('Page changed: $value');
+          },
+        ),
+      ),
+    );
+  }
+}
