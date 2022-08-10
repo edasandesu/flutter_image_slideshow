b153c65bfce4bd0fd6b109db814fbd1ccd058b99
edasandesu <nowtimber@gmail.com>
2022-08-10 22:47:45 +0900

    add src folder

---
+++ lib/src/image_slideshow.dart
@@ -0,0 +1,166 @@
+import 'dart:async';
+
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_image_slideshow/src/indicator.dart';
+
+class ImageSlideshow extends StatefulWidget {
+  const ImageSlideshow({
+    Key? key,
+    required this.children,
+    this.width = double.infinity,
+    this.height = 200,
+    this.initialPage = 0,
+    this.indicatorColor,
+    this.indicatorBackgroundColor = Colors.grey,
+    this.onPageChanged,
+    this.autoPlayInterval,
+    this.isLoop = false,
+  }) : super(key: key);
+
+  /// The widgets to display in the [ImageSlideshow].
+  ///
+  /// Mainly intended for image widget, but other widgets can also be used.
+  final List<Widget> children;
+
+  /// Width of the [ImageSlideshow].
+  final double width;
+
+  /// Height of the [ImageSlideshow].
+  final double height;
+
+  /// The page to show when first creating the [ImageSlideshow].
+  final int initialPage;
+
+  /// The color to paint the indicator.
+  final Color? indicatorColor;
+
+  /// The color to paint behind th indicator.
+  final Color? indicatorBackgroundColor;
+
+  /// Called whenever the page in the center of the viewport changes.
+  final ValueChanged<int>? onPageChanged;
+
+  /// Auto scroll interval.
+  ///
+  /// Do not auto scroll when you enter null or 0.
+  final int? autoPlayInterval;
+
+  /// Loops back to first slide.
+  final bool isLoop;
+
+  @override
+  ImageSlideshowState createState() => ImageSlideshowState();
+}
+
+class ImageSlideshowState extends State<ImageSlideshow> {
+  final _currentPageNotifier = ValueNotifier(0);
+  late PageController _pageController;
+  Timer? _timer;
+
+  void _onPageChanged(int index) {
+    _currentPageNotifier.value = index;
+    if (widget.onPageChanged != null) {
+      final correctIndex = index % widget.children.length;
+      widget.onPageChanged!(correctIndex);
+    }
+  }
+
+  void _autoPlayTimerStart() {
+    _timer?.cancel();
+    _timer = Timer.periodic(
+      Duration(milliseconds: widget.autoPlayInterval!),
+      (timer) {
+        int nextPage;
+        if (widget.isLoop) {
+          nextPage = _currentPageNotifier.value + 1;
+        } else {
+          if (_currentPageNotifier.value < widget.children.length - 1) {
+            nextPage = _currentPageNotifier.value + 1;
+          } else {
+            return;
+          }
+        }
+
+        goToPage(nextPage);
+      },
+    );
+  }
+
+  void goToPage(int index) {
+    if (_pageController.hasClients) {
+      _pageController.animateToPage(
+        index,
+        duration: const Duration(milliseconds: 350),
+        curve: Curves.easeIn,
+      );
+    }
+  }
+
+  void stopAutoPlay() {
+    _timer?.cancel();
+  }
+
+  @override
+  void initState() {
+    _pageController = PageController(
+      initialPage: widget.initialPage,
+    );
+    _currentPageNotifier.value = widget.initialPage;
+
+    if (widget.autoPlayInterval != null && widget.autoPlayInterval != 0) {
+      _autoPlayTimerStart();
+    }
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: widget.width,
+      height: widget.height,
+      child: Stack(
+        children: [
+          PageView.builder(
+            scrollBehavior: const ScrollBehavior().copyWith(
+              scrollbars: false,
+              dragDevices: {
+                PointerDeviceKind.touch,
+                PointerDeviceKind.mouse,
+              },
+            ),
+            onPageChanged: _onPageChanged,
+            itemCount: widget.isLoop ? null : widget.children.length,
+            controller: _pageController,
+            itemBuilder: (context, index) {
+              final correctIndex = index % widget.children.length;
+              return widget.children[correctIndex];
+            },
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 10,
+            child: ValueListenableBuilder<int>(
+              valueListenable: _currentPageNotifier,
+              builder: (context, value, child) {
+                return Indicator(
+                  count: widget.children.length,
+                  currentIndex: value % widget.children.length,
+                  activeColor: widget.indicatorColor,
+                  backgroundColor: widget.indicatorBackgroundColor,
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
